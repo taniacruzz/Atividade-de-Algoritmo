166d80448911bff26f468db7b594d2a733017a87
Isadora Rocha da Silva <isasilva0123@gmail.com>
2022-10-29 20:24:56 -0300

    Atualizando exercício 5

--- p1_exercicio5.py
+++ p1_exercicio5.py
@@ -1,9 +1,6 @@
 #exercício 5 da p1 
-soma_par_h = 0
-soma_impar_h = 0
-soma_par_s = 0
-soma_impar_s = 0
-
+soma_h = 0 
+soma_s = 0 
 soma_p = 0
 numero_primo = 1
 numero_impar = 1
@@ -11,7 +8,7 @@ numero_impar = 1
 n = int(input('N = ')) # o usuário vai dar um número para n
 
 if n >= 50: # verificando se n tem valor >= 50
-    for i in range(n):# para um intervalo de 0 a n: 
+    for i in range(n):# para um intervalo de 0 a (n-1): 
         #Calculando P:
         auxiliar = 0
         while auxiliar == 0:
@@ -25,22 +22,15 @@ if n >= 50: # verificando se n tem valor >= 50
         soma_p += (numero_primo/(numero_impar**3))
         numero_impar += 2
         #Calculando H e S:
-        while n > 1:
-            soma_h = 1 #começa a soma com 1, pois vamos indicar se n é par ou impar e para 1/1 sempre será positivo
-            soma_s = 1 # o mesmo caso de cima
-            if n % 2 == 0:
-                soma_par_h += (n*2-1)/n
-                soma_par_s += -(n/(n*n))
-            if n % 2 != 0:
-                soma_impar_h += -((n*2-1)/n)
-                soma_impar_s += n/(n*n)
-            soma_h += soma_par_h + soma_impar_h
-            soma_s += soma_par_s + soma_impar_s
-            n -= 1
-    #SAÍDAS:
-    #print('\nH:\nsoma par {}\nsoma impar {}\nsoma {}\n'.format(soma_par_h, soma_impar_h, soma_h))
-    #print('S:\nsoma par {}\nsoma impar {}\nsoma {}\n'.format(soma_par_s, soma_impar_s, soma_s))
-    #print('P:\nsoma {}\n'.format(soma_p))
+        if i == 0:
+            soma_h = 1
+            soma_s = 1
+        if i % 2 == 0 and i !=0:
+            soma_h += -((i+1)*2-1)/(i+1)
+            soma_s += ((i+1)/((i+1)*(i+1)))
+        if i % 2 != 0:
+            soma_h += (((i+1)*2-1)/(i+1))
+            soma_s += -((i+1)/((i+1)*(i+1)))
     print('H = {}\nS = {}\nP = {}\n'.format(soma_h, soma_s, soma_p))
 else:
     print('N tem que ser maior ou igual a 50')
