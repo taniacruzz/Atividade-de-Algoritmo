b0843a1edcfa90e89049aa98060b0a4da82b8e33
Isadora Rocha da Silva <isasilva0123@gmail.com>
2022-10-29 18:41:36 -0300

    Atualização da atividade 5

--- p1_exercicio5.py
+++ p1_exercicio5.py
@@ -11,8 +11,8 @@ numero_impar = 1
 n = int(input('N = ')) # o usuário vai dar um número para n
 
 if n >= 50: # verificando se n tem valor >= 50
-    #Calculando P:
-    for i in range(n):# para um intervalo de 1 a n: 
+    for i in range(n):# para um intervalo de 0 a n: 
+        #Calculando P:
         auxiliar = 0
         while auxiliar == 0:
             quantidade_divisores = 0
@@ -24,19 +24,19 @@ if n >= 50: # verificando se n tem valor >= 50
                 auxiliar = 1
         soma_p += (numero_primo/(numero_impar**3))
         numero_impar += 2
-    #Calculando H e S:
-    while n > 1:
-        soma_h = 1 #começa a soma com 1, pois vamos indicar se n é par ou impar e para 1/1 sempre será positivo
-        soma_s = 1 # o mesmo caso de cima
-        if n % 2 == 0:
-            soma_par_h += (n*2-1)/n
-            soma_par_s += -(n/(n*n))
-        if n % 2 != 0:
-            soma_impar_h += -((n*2-1)/n)
-            soma_impar_s += n/(n*n)
-        soma_h += soma_par_h + soma_impar_h
-        soma_s += soma_par_s + soma_impar_s
-        n -= 1
+        #Calculando H e S:
+        while n > 1:
+            soma_h = 1 #começa a soma com 1, pois vamos indicar se n é par ou impar e para 1/1 sempre será positivo
+            soma_s = 1 # o mesmo caso de cima
+            if n % 2 == 0:
+                soma_par_h += (n*2-1)/n
+                soma_par_s += -(n/(n*n))
+            if n % 2 != 0:
+                soma_impar_h += -((n*2-1)/n)
+                soma_impar_s += n/(n*n)
+            soma_h += soma_par_h + soma_impar_h
+            soma_s += soma_par_s + soma_impar_s
+            n -= 1
     #SAÍDAS:
     #print('\nH:\nsoma par {}\nsoma impar {}\nsoma {}\n'.format(soma_par_h, soma_impar_h, soma_h))
     #print('S:\nsoma par {}\nsoma impar {}\nsoma {}\n'.format(soma_par_s, soma_impar_s, soma_s))
